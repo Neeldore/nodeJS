f6f4d5e7af201c5e8f32b8b31fee90a3b7c90013
neeldore <neeljain14@gmail.com>
2018-04-04 17:16:32 +0530

    port fix

--- HelloWorld/router.js
+++ HelloWorld/router.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     var cryptoWatch = require('./cryptoWatch/getCryptoValue');
-    app.get('/searchme', function (req, res) {
-        console.log('im working')
+    app.get('/', function (req, res) {
+        cryptoWatch.getPrices(res);
     })
 }
